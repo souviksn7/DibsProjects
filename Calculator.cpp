d0fbd5e674e398859fee58b6343bf9d603c8c961
DIBYENDU MAJI <32224234+dibyendu415@users.noreply.github.com>
2019-12-26 17:27:07 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,20 +1,20 @@
-Basic Calculator using C++
+//Basic Calculator using C++
 
 # include <iostream>
 using namespace std;
 int main()
 {
-    int ch;
+    int ch,sum;
     float num1, num2;
-    cout << "Enter option 1.Addition \t 2. Subtraction \n 3.Multiplication \t 4. Division \n 5. Modulus \t 6. Maximum of two nos  ";
+    cout << "Enter option \n 1.Addition \t 2. Subtraction \n 3.Multiplication \t 4. Division \n 5. Modulus \t 6. Maximum of two nos \n ";
     cin >> ch;
     cout << "Enter two operands: ";
     cin >> num1 >> num2;
     switch(ch)
     {
-        case '1':
+        case 1:
             sum=num1+num2;
-            cout<<"Sum of two numbers is "<<sum
+            cout<<"Sum of two numbers is "<<sum;
             break;
         default:
             // If the options is not 1-6, error message is shown
