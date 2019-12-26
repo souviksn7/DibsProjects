3f2e83cbd3fca31ac7e45a2de4d9286debbbc315
DIBYENDU MAJI <32224234+dibyendu415@users.noreply.github.com>
2019-12-26 19:56:21 +0530

    Create Age_Calculator.cpp

---
+++ Age_Calculator.cpp
@@ -0,0 +1,26 @@
+//Program to check your age till date
+
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int birth_month,birth_year;
+int current_month,current_year;
+int ageinyear,ageinmonth;
+cout<<"Age Calculator\n\n";
+
+cout<<"\nEnter Your Birth Month(Eg:8): ";
+cin>>birth_month;
+cout<<"\nEnter Your Birth Year(Eg:1999): ";
+cin>>birth_year;
+
+cout<<"\nEnter The Current Month(Eg:12): ";
+cin>>current_month;
+cout<<"\nEnter The Current Year(Eg:2020): ";
+cin>>current_year;
+ageinyear= ;
+ageinmonth= ;
+cout<<"\n\nYour Age is "<<ageinyear<<" Years And "<<ageinmonth<<" Months ";
+getch();
+}
